32483dc043ae3c401147cf8e38594d655d8a5f19
mateus-t-estagio <57234792+mateus-t-estagio@users.noreply.github.com>
2019-11-07 15:22:13 -0300

    Rename app.py to app1.py

--- app1.py
+++ app1.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore")
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
-
+server = app.server
 # Leitura dos arquivos em excel
 #Leitura da base de dados para a Produção, tanto em TU quanto em TKU
 Producao=pd.read_excel('Producao.xlsx')
